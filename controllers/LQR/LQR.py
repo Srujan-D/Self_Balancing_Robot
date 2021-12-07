832ef2077daf07e61eda732b1a0e17a2b9d492fd
Srujan Deolasee <53463585+Srujan-D@users.noreply.github.com>
2021-12-07 12:32:13 +0530

    added planner (A*)

--- controllers/LQR/LQR.py
+++ controllers/LQR/LQR.py
@@ -1,6 +1,10 @@
+from typing import Type
 from controller import Robot, Keyboard, Supervisor,Emitter,Receiver,PositionSensor, InertialUnit, GPS
 from math import atan, pi, acos, sqrt, sin, cos, atan2
 import numpy as np
+import heapq
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 robot = Robot()
 TIMESTEP = int(robot.getBasicTimeStep())
@@ -19,14 +23,13 @@ def setSpeed(l,r):
     motor[1].setVelocity(r)
 
 state = [0, 0, 0, 0]
+
+# static gain matrix
 K = [4., -2.7, 122, 550]
 
-# K = [30, -5.7, 350, 900] # works for standing still, not for going to a point
-# K = [70, -6, 123, 800] # works for lDis = rDis = 460 rs.
-
-def lqrSpeed(phi, dphi, theta, theta_dot):
-    state[0] = phi
-    state[1] = dphi
+def lqrSpeed(x, dx, theta, theta_dot):
+    state[0] = x
+    state[1] = dx
     state[2] = theta 
     state[3] = theta_dot
     
@@ -53,14 +56,12 @@ for i in range(len(motorNames)):
 
 
 #Setup IMU
-# imu = robot.getDevice('InertialUnit')
 imu = InertialUnit("inertial unit")
 imu.enable(TIMESTEP)
 
 #Setup GPS
 gps = GPS('gps')
 gps.enable(TIMESTEP)
-print(gps.getValues())
 
 #Setup Accelerometer and Gyroscope
 accMeter = robot.getDevice('accel')
@@ -85,10 +86,151 @@ lDisRef = lEnc.getValue()
 rDisRef = rEnc.getValue()
 
 stop = False
-xD = 0
+xD = 0 
 
-path = [[0.125,0.],[0.125,0.125]]
-path = [(-1.125, -1.125), (-0.875, -0.875), (-0.625, -0.625), (-0.375, -0.375), (-0.125, -0.125), (-0.125, 0.125), (-0.125, 0.375), (0.125, 0.625), (0.375, 0.875), (0.625, 1.125), (0.875, 1.125), (1.125, 1.125), (1.375, 1.375)]
+def map_return(x, y):
+	
+	n = x
+	m = y
+
+	grid = [[0 for i in range(n)] for j in range (m)]
+
+	grid [0][0] = 0
+	grid [11][11] = 0
+
+	grid[2][11] = 1
+	grid[2][10] = 1
+	grid[2][4] = 1
+	grid[2][5] = 1
+	grid[3][11] = 1
+	grid[3][10] = 1
+	grid[3][4] = 1
+	grid[3][5] = 1
+	grid[4][2] = 1
+	grid[4][3] = 1
+	grid[5][2] = 1
+	grid[5][3] = 1
+
+	grid[6][6] = 1
+	grid[6][7] = 1
+	grid[6][8] = 1
+	grid[6][9] = 1
+	grid[7][6] = 1
+	grid[7][7] = 1
+	grid[7][8] = 1
+	grid[7][9] = 1
+
+	grid[8][1] = 1
+	grid[8][2] = 1
+	grid[9][1] = 1
+	grid[9][2] = 1
+
+	grid[10][3] = 1
+	grid[10][4] = 1
+	grid[11][3] = 1
+	grid[11][4] = 1
+
+
+	grid = np.array(grid)
+
+	return grid
+
+def heuristic(a, b, type = "euclidean"):
+	if type == "euclidean":
+		return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+	elif type == "manhattan":
+		return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def check_neighbours(array, neighbor):
+	if array[neighbor[0]][neighbor[1]] == 1:
+		return True
+	else:
+		return False
+
+def astar(array, start, goal, heuristic_type):
+
+	neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+	close_set = set()
+	came_from = {}
+	gscore = {start:0}
+	fscore = {start:heuristic(start, goal, heuristic_type)}
+	oheap = []
+
+	heapq.heappush(oheap, (fscore[start], start))
+ 
+	while oheap:
+
+		current = heapq.heappop(oheap)[1]
+		if current == goal:
+			data = []
+			while current in came_from:
+				data.append(current)
+				current = came_from[current]
+			return data
+		close_set.add(current)
+		for i, j in neighbors:
+			neighbor = current[0] + i, current[1] + j
+			tentative_g_score = gscore[current] + heuristic(current, neighbor, heuristic_type)
+			if 0 <= neighbor[0] < array.shape[0]:
+				if 0 <= neighbor[1] < array.shape[1]:
+					if check_neighbours(grid, neighbor):
+						continue
+				else:
+                    # array bound y walls
+					continue
+			else:
+				# array bound x walls
+				continue
+
+			if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+				continue
+
+			if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+				came_from[neighbor] = current
+				gscore[neighbor] = tentative_g_score
+				fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal, heuristic_type)
+				heapq.heappush(oheap, (fscore[neighbor], neighbor))
+
+	return False
+
+def path_return(route):
+
+	# route = route + [start]
+
+	route = route[::-1]
+	#extract x and y coordinates from route list
+
+	x_coords = []
+	y_coords = []
+
+	path = []
+	for i in (range(0,len(route))):
+		x = route[i][0]*0.25 - 1.375
+		y = route[i][1]*0.25 - 1.375
+		# x = route[i][0]
+		# y = route[i][1]
+		x_coords.append(x)
+		y_coords.append(y)
+		path.append((y,x))
+	# print(path)
+	return path
+# plot map and path
+
+def plot_path(grid, start, goal, x_coords, y_coords):
+	fig, ax = plt.subplots(figsize=(20,20))
+	ax.imshow(grid, cmap=plt.cm.Dark2)
+	ax.scatter(start[1],start[0], marker = "*", color = "yellow", s = 200)
+	ax.scatter(goal[1],goal[0], marker = "*", color = "red", s = 200)
+	ax.plot(y_coords,x_coords, color = "black")
+	plt.show()
+
+start = (0,0)
+goal = (11,11)
+
+grid = map_return(12, 12)
+route = astar(grid, start, goal, heuristic_type="euclidean")
+path = path_return(route)
+
 theta_setpoint = None
 setpoint = None
 
@@ -126,9 +268,6 @@ while robot.step(timestep) != -1:
 
 		xD = (lDis+rDis)*90/pi # total rotation in degree
 		
-		# dist = xD*0.025*pi/90
-		# print("dist is : ", xD*0.025*pi/90)
-		
 		if (robot.getTime()-oldT)>0:
 			speed = lqrSpeed(xD, (oldxD-xD)/(robot.getTime()-oldT), angle, g_x)
 		else:
@@ -138,9 +277,6 @@ while robot.step(timestep) != -1:
 			
 	elif stop:
 		speed = speed
-
-	#print(speed)
-	# print(yaw)
 	
 	try:
 		if setpoint != path[0]:
@@ -150,6 +286,8 @@ while robot.step(timestep) != -1:
 		setpoint = (z_pos, x_pos)
 		theta_setpoint = yaw
 		# stop = True
+	except TypeError:
+		print("path is ", path)
 	print(yaw, theta_setpoint, setpoint)
 	if not stop and abs(theta_setpoint - yaw) > 0.005:	# for clockwise
 		print("turn, dont travel")
@@ -177,4 +315,4 @@ while robot.step(timestep) != -1:
 			# print("Falling..............")
 			setSpeed(speed, speed)
 			continue
-		# setSpeed(speed,speed)    
+		# setSpeed(speed,speed)
